e7d42262cc259681fc8fe71a2371a7ea21c5e67b
Josh Keller <jrkeller@gmail.com>
2024-06-26 15:46:18 -0400

    Set 2 Challenge 9

---
+++ set1/c9.go
@@ -0,0 +1,14 @@
+package set1
+
+import (
+	"bytes"
+)
+
+func PadString(s string, blocksize int) string {
+	if blocksize > 255 {
+		panic("Cannot pad block more than 255")
+	}
+
+	padSize := blocksize - (len(s) % blocksize)
+	return s + string(bytes.Repeat([]byte{uint8(padSize)}, padSize))
+}
--- set1/set1_test.go
+++ set1/set1_test.go
@@ -108,3 +108,20 @@ func TestDetectAESECB(t *testing.T) {
 		assert.Equal(t, "d880", string(ecbLines[0][0:4]))
 	})
 }
+
+func TestPKCS7Padding(t *testing.T) {
+	t.Run("PKCS#7 padding", func(t *testing.T) {
+		cases := []struct {
+			input     string
+			blocksize int
+			expected  string
+		}{
+			{"YELLOW SUBMARINE", 20, "YELLOW SUBMARINE\x04\x04\x04\x04"},
+			{"YELLOW SUBMARINE", 16, "YELLOW SUBMARINE\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10"},
+		}
+		for _, tc := range cases {
+			padded := PadString(tc.input, tc.blocksize)
+			assert.Equal(t, tc.expected, padded)
+		}
+	})
+}
